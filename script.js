00c61941aff4afef5a1a068cc882e866789857de
Leon Birch <leonbirch333@gmail.com>
2024-06-20 14:24:52 +0100

    created document query selector function

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 "use strict";
+
+const docQS = function (selector) {
+  return document.querySelector(selector);
+};
+
+const modal = docQS(".modal");
+const overlay = docQS(".overlay");
+const btn = docQS(".close-modal");
+const btnsOpenModal = docQS(".open-modal");
