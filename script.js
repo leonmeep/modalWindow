4490e745b0f123af8fc6ea9f4ef0fecd75dc3648
Leon Birch <leonbirch333@gmail.com>
2024-06-20 15:20:33 +0100

    added click event listener for each button, and created functions to remove and add the hidden property

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 "use strict";
-
+//query selector functions
 const docQS = function (selector) {
   return document.querySelector(selector);
 };
@@ -7,11 +7,25 @@ const docQSA = function (selector) {
   return document.querySelectorAll(selector);
 };
 
+//classes to manipulate
 const modal = docQS(".modal");
 const overlay = docQS(".overlay");
-const btn = docQS(".close-modal");
+const btnCloseModal = docQS(".close-modal");
 const btnsOpenModal = docQSA(".show-modal");
-console.log(btnsOpenModal);
 
-for (let i = 0; 1 < btnsOpenModal.length; i++)
-  console.log(btnsOpenModal[i].textContent);
+//function that removes hidden class on modal
+const removeHidden = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+//function that adds hidden class
+const addHidden = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener("click", removeHidden);
+
+btnCloseModal.addEventListener("click", addHidden);
+overlay.addEventListener("click", addHidden);
