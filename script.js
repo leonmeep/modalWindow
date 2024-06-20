33e68b9ad20fe9696fd5d4ec70daa13c85b019e4
Leon Birch <leonbirch333@gmail.com>
2024-06-20 14:32:39 +0100

    created function for query selector all

--- script.js
+++ script.js
@@ -3,8 +3,12 @@
 const docQS = function (selector) {
   return document.querySelector(selector);
 };
+const docQSA = function (selector) {
+  return document.querySelectorAll(selector);
+};
 
 const modal = docQS(".modal");
 const overlay = docQS(".overlay");
 const btn = docQS(".close-modal");
-const btnsOpenModal = docQS(".open-modal");
+const btnsOpenModal = docQSA(".show-modal");
+console.log(btnsOpenModal);
